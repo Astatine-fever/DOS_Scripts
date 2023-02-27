8f39e81f4e66198fb9eccf2428401908587c9614
Astatine <108997067+Astatine-fever@users.noreply.github.com>
2023-02-27 14:39:05 +0530

    Test file

--- loading.bat
+++ loading.bat
@@ -1,44 +1,23 @@
 @echo off
-:loading
-    call:Stage1
-    ping /n 1 /w 2000 localhost >nul
-    cls
-    ping /n 1 /w 4000 localhost >nul
-    call:Stage2
-    ping /n 1 /w 4000 localhost >nul
-    cls
-    ping /n 1 /w 6000 localhost >nul
-    call:Stage3
-    ping /n 1 /w 6000 localhost >nul
-    cls
-    ping /n 1 /w 8000 localhost >nul
-    call:Stage4
-    ping /n 1 /w 8000 localhost >nul
-    cls
-    ping /n 1 /w 2000 localhost >nul
-    call:loading
 
+echo "   _________ "
+echo " //         | "
+echo " T          | "
+echo " |   Moving | "
+echo " |          | "
+echo " |__________| "
 
-:Stage1
-    echo  .--.                         
-    echo ( _.-' .-.   .-.  .-.   .''.  
-    echo (  '-. '-'   '-'  '-'   '..'  
-    echo  '--'                         
+echo "                ________  "
+echo "             //         | "
+echo " ------------T          | "
+echo " ------------|   Moving | "
+echo " ------------|          | "
+echo " ------------|__________| "
 
-:Stage2
-    echo       .--.                         
-    echo      ( _.-' .-.  .-.    .''.  
-    echo      (  '-. '-'  '-'    '..'  
-    echo       '--'                         
+echo "                             ________  "
+echo "                          //         | "
+echo " -------------------------T          | "
+echo " -------------------------|  Moving  | "
+echo " -------------------------|          | "
+echo " -------------------------|__________| "
 
-:Stage3
-    echo                  .--.                         
-    echo                 ( _.-'   .''.  
-    echo                 (  '-.   '..'  
-    echo                  '--'                         
-
-:Stage4
-    echo                           .--.                         
-    echo                          ( _.-'   
-    echo                          (  '-.   
-    echo                           '--'                         
